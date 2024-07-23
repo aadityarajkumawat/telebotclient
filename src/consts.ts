028381d5c182f19d02f0b30afd715f3142eb9105
Aditya Raj Kumawat <arkumawat78@gmail.com>
2024-07-23 12:31:42 -0400

    combined bot and server, updated the URL

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,1 @@
-export const BASE_URL =
-  "https://triviabot-admin-server-production.up.railway.app/api";
+export const BASE_URL = "https://triviabot-production.up.railway.app";
---
+++ src/zodFetch.ts
@@ -0,0 +1,38 @@
+import z from "zod";
+
+export async function safeFetch<T>(
+  schema: z.Schema<T>,
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<T> {
+  const response = await fetch(input, init);
+
+  if (!response.ok) {
+    throw newHTTPError("Unsuccessful response", response, init?.method);
+  }
+
+  const json = await response.json().catch(() => {
+    throw newHTTPError("Not a JSON body", response, init?.method);
+  });
+
+  const result = schema.safeParse(json);
+  if (!result.success) {
+    throw newHTTPError("Unexpected response schema", response, init?.method);
+  }
+
+  return result.data;
+}
+
+function newHTTPError(reason: string, response: Response, method?: string) {
+  const text = response.text().catch(() => null);
+  const message = `Error fetching ${method} ${response.url} ${response.status}.${reason}`;
+  console.error(`${message} Response body: ${text}`);
+  return new HTTPError(response.status, message);
+}
+
+export class HTTPError extends Error {
+  constructor(public status: number, message: string) {
+    super(message);
+    this.status = status;
+  }
+}
